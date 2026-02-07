72d165743b7dd8ec036deade669fbb9d56e8b6b5
BrayneLopezz <lopezbrainer98@gmail.com>
2026-02-06 20:29:43 -0500

    Implementacion de Pytest/Tests

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from app.products.repository.log_data import LogDataBase
 from app.products.repository.data_base import ProductDateBase
 from app.products.validators.register_validation import ValidationProductRegistration
 from app.products.service.insert_history import ValidationProductsState
-from app.constants.product_const.assignment_key import NAME_IN_KEY, NAME_ON_KEY_ERROR
+from app.products.constants import NAME_IN_KEY, NAME_ON_KEY_ERROR
 def run():
     log = LogDataBase()
     db = ProductDateBase()
-    vn = ValidationProductsState(log, db, NAME_IN_KEY, NAME_ON_KEY_ERROR)
-    register = ValidationProductRegistration(db, 1275358, 'Gaseosa Manzana 2L', 4500, '28/01/2026', 'Bebida')
+    vp = ValidationProductsState(log, db, NAME_IN_KEY, NAME_ON_KEY_ERROR)
+    register = ValidationProductRegistration(db, 12753958, 'Gaseosa Manzana 2L', 0, '28/01/2026', 'Bebida')
     
     new_product_implemented = register.orquest()
-    vn.log_insert_validation(register.code, new_product_implemented)
+    vp.log_insert_validation(register.code, new_product_implemented)
     
     print(register.orquest())
     print(log.registration_successful)
